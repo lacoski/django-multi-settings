adb18586f46ca1c7cbef5ef51fecbdc796aa2369
lacoski <123xuzu@gmail.com>
2018-09-29 17:10:43 +0700

    update

--- src/project/authen/views.py
+++ src/project/authen/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from .models import User
 
 # Create your views here.
 
--- src/project/project/settings.py
+++ src/project/project/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'authen',
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authen.apps.AuthenConfig',
 ]
 
 MIDDLEWARE = [
